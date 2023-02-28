d0a5da31f0abe571d061a106c8a8f56ba125433c
mrkenSK2 <nakamura.k.cb@m.titech.ac.jp>
2023-02-28 18:38:55 +0900

    contributeされなかった

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,13 +1,13 @@
 buttonClick = function(){
     alert("mes");
 }
-/*function inputChange(){
+function inputChange(){
     let element = document.getElementById('q_num');
     if(!isNaN(element.value)) {
         alert("miss");
 
     }
-}*/
+}
 checkPositive = function() {
     let q_num = parseInt(document.qinfo.q_num.value);
     if (q_num <= 0) {
