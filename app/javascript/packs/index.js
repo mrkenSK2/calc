557e3ea280555b0c2cfdb16aab4f3c0fd7757cc7
mrkenSK2 <nakamura.k.cb@m.titech.ac.jp>
2023-03-01 19:05:52 +0900

    問題数空白無効

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,15 +1,13 @@
 buttonClick = function(){
     alert("mes");
 }
-function inputChange(){
-    let element = document.getElementById('q_num');
-    if(!isNaN(element.value)) {
-        alert("miss");
 
-    }
-}
-checkPositive = function() {
+checkQnum = function() {
     let q_num = parseInt(document.qinfo.q_num.value);
+    if (isNaN(q_num)) {
+        alert("問題数を入力してください");
+        return false;
+    }
     if (q_num <= 0) {
         alert("正の整数を入力してください");
         return false;
