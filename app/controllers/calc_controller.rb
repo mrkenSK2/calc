6cf2d60373cb76d4f588c57a3311a4a010669fcd
mrkenSK2 <nakamura.k.cb@m.titech.ac.jp>
2023-03-17 23:09:25 +0900

    form_for使う

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -5,16 +5,18 @@ class CalcController < ApplicationController
     
     def sol
         @q_num = params[:q_num].to_i
+        @score = Score.new
     end
         
     def create
         if request.post? then
-            obj = Score.create(
-                user: "sa",
-                total: params[:q_num],
-                correct: params[:correct]
-            )
+            Score.create(score_params)
         end        
         redirect_to "/calc"
+    end
+
+    private
+    def score_params
+        params.require(:score).permit(:user, :total, :correct)
     end
 end
